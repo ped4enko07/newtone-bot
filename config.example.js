a1735b257be9d0d1fa0222750fbf7fd273fb7368
Ped4enko Roman <ped4enko07@gmail.com>
2017-09-16 00:44:59 +0300

    create config.example

---
+++ config.example.js
@@ -0,0 +1,4 @@
+const APPNAME = 'name_application';
+module.exports = require('rc')(APPNAME, {
+  token: 'token to telegram',
+});
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const {token} = require('./config');
+const { token } = require('./config');
 
+const TelegramBot = require('node-telegram-bot-api');
 
-console.log(token);
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const fromId = msg.from.id;
+  const resp = match[1];
+  bot.sendMessage(fromId, resp);
+});
