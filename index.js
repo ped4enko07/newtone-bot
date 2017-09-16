cb273533e6412f77789c7d2ee191e877f228ff0c
Ped4enko Roman <ped4enko07@gmail.com>
2017-09-16 23:57:17 +0300

    add nodemon

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.on('text', (msg) => {
   const fromId = msg.from.id;
-  const resp = match[1];
-  bot.sendMessage(fromId, resp);
+  bot.sendMessage(fromId, `${msg.from} hello world!`);
 });
+
